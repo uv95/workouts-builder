d6e2c2b44c6ce1d3ebe0f5044e3cacb7ff7d6799
Uliana <ulianav95v@gmail.com>
2022-07-11 15:43:03 +0300

    Added hero bg image

--- src/components/BodyPart.jsx
+++ src/components/BodyPart.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 
 function BodyPart({ name, children, checked, onChange, onClick }) {
   return (
-    <div onClick={onClick} className="collapse collapse-arrow">
+    <div className="collapse collapse-arrow">
       <input
         id={name}
         className="h-14"
         type="checkbox"
         checked={checked}
         onChange={onChange}
+        onClick={onClick}
       />
       <div className="collapse-title py-0 flex items-center text-2xl">
         {name}
--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import fitness4 from '../assets/svg/fitness4.jpg';
 
 function Hero() {
   return (
-    <>
-      <h1 className="w-1/2 mt-60 text-8xl font-bold text-center">
-        BUILD YOUR WORKOUTS
-      </h1>
-      <div className="flex gap-6 mt-5">
-        <Link to="/exercises" className="btn btn-secondary w-40">
-          Exercises
-        </Link>
-        <Link to="/sign-in" className="btn btn-primary w-40">
+    <div
+      style={{ backgroundImage: `url(${fitness4})` }}
+      className="w-screen bg-cover hero min-h-screen"
+    >
+      <div className="min-h-screen bg-white/25 w-full flex flex-col justify-center items-center text-center gap-6">
+        <h1 className="w-1/2 -mt-64 text-8xl font-bold z-10">
+          BUILD YOUR WORKOUTS
+        </h1>
+        <Link
+          to="/exercises"
+          className="btn btn-primary w-40 z-10 mt-5 shadow-xl shadow-white/25"
+        >
           build workout
         </Link>
       </div>
-    </>
+    </div>
   );
 }
 
